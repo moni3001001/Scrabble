a1c3704760641ab0f96b9b83a3618fb32778e631
Bhargava <subhargava@paypalcorp.com>
2016-08-05 12:42:14 +0530

    added dictionary formation, main and isValidScrabble.java

--- src/Scrabble.java
+++ src/Scrabble.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class Scrabble {
+	        
 			String letters;
-			public boolean isValid(String s){
-				
+			
+			public static ArrayList<String> readFile(String path) throws Exception {
+				return (ArrayList<String>) Files.readAllLines(Paths.get(path));
 			}
+			
+			public boolean isValid(String s, ArrayList<String> dict){
+				return dict.contains(s);
+			}
+			
 			public int scoreLetter(char c){
 				switch(c){
 				case 'A':
@@ -11,10 +24,19 @@ public class Scrabble {
 				case 'B'
 				}
 			}
+			
 			public int maxScore(){
 				return score;
 			}
+			
+			
 			public static void main(String args[]){
+				string input = "SCHOOLS";
+				String filePath = "C:\\Users\\subhargava\\Downloads\\sowpods.txt";
+				ArrayList<String> dictionary = new ArrayList<String> ();
+				dictionary = readFile(filePath);
+				
+				
 				
 			}
 }
